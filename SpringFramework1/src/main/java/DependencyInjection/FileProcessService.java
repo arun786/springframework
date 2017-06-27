b31655026a17b03fc816f8e99d9a1388aa2d5254
arun789 <adwiti1975@gmail.com>
2017-06-27 00:54:11 -0700

    adding logs

--- SpringFramework1/src/main/java/DependencyInjection/FileProcessService.java
+++ SpringFramework1/src/main/java/DependencyInjection/FileProcessService.java
@@ -2,6 +2,7 @@ package DependencyInjection;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,13 @@ public class FileProcessService implements IFileProcessingService {
 	@Autowired
 	private IFileReadService frs;
 	
+	Logger logger = Logger.getLogger(FileProcessService.class);
+	
 	@Value("${report.filename}")
 	private String filename;
 
 	public void processAFile() {
+		logger.info("Processing the file");
 		List<String> lstOfLines = frs.readAFile(filename);
 		lstOfLines.forEach(System.out::println);
 	}
--- SpringFramework1/src/main/java/DependencyInjection/FileReadService.java
+++ SpringFramework1/src/main/java/DependencyInjection/FileReadService.java
@@ -6,15 +6,19 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileReadService implements IFileReadService {
+	
+	Logger logger = Logger.getLogger(FileReadService.class);
 
 	/**
 	 * It reads a file line by line and stores in a list
 	 */
 	public List<String> readAFile(String filename) {
+		logger.info("Reads a file");
 		List<String> lstFiles = new ArrayList<String>();
 		try {
 			lstFiles = Files.readAllLines(Paths.get(filename));
