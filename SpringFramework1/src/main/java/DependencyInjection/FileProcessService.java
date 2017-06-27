3de40615ad5b90ef2675dfae26646c39c89ef2d1
arun789 <adwiti1975@gmail.com>
2017-06-27 00:40:32 -0700

    to read a property file

--- SpringFramework1/src/main/java/DependencyInjection/FileProcessService.java
+++ SpringFramework1/src/main/java/DependencyInjection/FileProcessService.java
@@ -3,6 +3,7 @@ package DependencyInjection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,9 +11,12 @@ public class FileProcessService implements IFileProcessingService {
 
 	@Autowired
 	private IFileReadService frs;
+	
+	@Value("${report.filename}")
+	private String filename;
 
 	public void processAFile() {
-		List<String> lstOfLines = frs.readAFile("Cartier+7-day+auctions.csv");
+		List<String> lstOfLines = frs.readAFile(filename);
 		lstOfLines.forEach(System.out::println);
 	}
 }
--- SpringFramework1/src/main/java/DependencyInjection/FileReadingApp.java
+++ SpringFramework1/src/main/java/DependencyInjection/FileReadingApp.java
@@ -2,13 +2,23 @@ package DependencyInjection;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan
+@PropertySource("classpath:application.properties")
 public class FileReadingApp {
+
 	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(FileReadingApp.class);
 		IFileProcessingService fps = (IFileProcessingService) context.getBean("fileProcessService");
